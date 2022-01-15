f296dbbd560e66d0f36bd4b484e1f7e08ad7c2ce
Raoul Wols <raoulwols@gmail.com>
2022-01-16 00:25:23 +0100

    Account for an empty selection

--- show_char_code.py
+++ show_char_code.py
@@ -10,6 +10,10 @@ class ShowCharCodeCommand(sublime_plugin.EventListener):
         self.show_char_code(view)
 
     def show_char_code(self, view):
-        selected = view.substr(view.sel()[0].a)
+        selection = view.sel()
+        if len(selection) == 0:
+            view.erase_status('charcode')
+            return
+        selected = view.substr(selection[0].a)
         view.set_status('charcode',
                         "DEC {0}, HEX {0:#x}, BYTE {1}".format(ord(selected), selected.encode('unicode_escape')))
